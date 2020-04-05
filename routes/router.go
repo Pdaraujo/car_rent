a4bd1bcd7d94899a8bd1748b3d802530cc13b772
Diogo Araújo <diogo.pdaraujo@gmail.com>
2020-04-05 01:36:49 +0100

    refactored navbar

--- routes/router.go
+++ routes/router.go
@@ -12,5 +12,10 @@ func NewRouter() *mux.Router {
 		home := views.NewView("bootstrap", "static/home")
 		home.Render(w, r, nil)
 	})
+	// Assets
+	assetHandler := http.FileServer(http.Dir("./assets/"))
+	assetHandler = http.StripPrefix("/assets/", assetHandler)
+	router.PathPrefix("/assets/").Handler(assetHandler)
+
 	return router
 }
